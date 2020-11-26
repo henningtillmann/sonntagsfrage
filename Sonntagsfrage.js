c44706da5eed0d96678ea39217db47580cc09dea
Henning Tillmann <github@service.henning-tillmann.de>
2020-11-26 17:13:46 +0100

    Update Sonntagsfrage.js

--- Sonntagsfrage.js
+++ Sonntagsfrage.js
@@ -1,6 +1,3 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: light-gray; icon-glyph: chart-bar;
 /************************************************
 
 Sonntagsfrage iOS Widget
